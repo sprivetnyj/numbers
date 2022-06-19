6c6ff29718aa1abf2375e3a56abbe297bd264b65
Nikita Korshunov <sprivetnyj@gmail.com>
2022-06-19 16:21:59 +0300

    5

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,15 +1,5 @@
 import sndClick from '../audio/click.wav';
 import sndConfetti from '../audio/confetti.wav';
-import sndEnd from '../audio/end.wav';
-
-import sndTap1 from '../audio/tap_1.wav';
-import sndTap2 from '../audio/tap_2.wav';
-import sndTap3 from '../audio/tap_3.wav';
-import sndTap4 from '../audio/tap_4.wav';
-import sndTap5 from '../audio/tap_5.wav';
-import sndTap6 from '../audio/tap_6.wav';
-import sndTap7 from '../audio/tap_7.wav';
-import sndTap8 from '../audio/tap_8.wav';
 
 export const audio = {
 	Click: new Howl({
@@ -19,41 +9,5 @@ export const audio = {
 	Confetti: new Howl({
 		src: './audio/confetti.wav',
 		html5: true
-	}),
-	End: new Howl({
-		src: './audio/end.wav',
-		html5: true
-	}),
-	Tap1: new Howl({
-		src: './audio/tap_1.wav',
-		html5: true
-	}),
-	Tap2: new Howl({
-		src: './audio/tap_2.wav',
-		html5: true
-	}),
-	Tap3: new Howl({
-		src: './audio/tap_3.wav',
-		html5: true
-	}),
-	Tap4: new Howl({
-		src: './audio/tap_4.wav',
-		html5: true
-	}),
-	Tap5: new Howl({
-		src: './audio/tap_5.wav',
-		html5: true
-	}),
-	Tap6: new Howl({
-		src: './audio/tap_6.wav',
-		html5: true
-	}),
-	Tap7: new Howl({
-		src: './audio/tap_7.wav',
-		html5: true
-	}),
-	Tap8: new Howl({
-		src: './audio/tap_8.wav',
-		html5: true
-	}),
+	})
 }
