bade5485e6b992ffd8c93c6d726196f6aaf037c3
Nikita Korshunov <sprivetnyj@gmail.com>
2022-06-22 14:23:19 +0300

    17

--- src/js/index.js
+++ src/js/index.js
@@ -89,8 +89,6 @@ vkBridge.send('VKWebAppStorageGet', { 'keys': ['lvlKey1', 'helpKey1', 'soundKey0
 		}, 2000);
 	});
 
-preloader.classList.add('hidden');
-
 // a b c d e f g h i j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z  0  1  2  3
 // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
 const digits = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3'];
